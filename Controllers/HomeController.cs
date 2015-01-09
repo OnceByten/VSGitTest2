719e6f53cfcc0259a99b4ed83eccae82e847a161
Moss Palmer <moss@oncebyten.com>
2015-01-09 17:40:02 +0000

    mp
    mp

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -9,13 +9,8 @@ namespace VSGitTest2.Controllers
     public class HomeController : Controller
     {
         //
-<<<<<<< HEAD
-        // GET: /Home/
-        // erm, no..!!
-=======
         // GET: /Home/ - by simon
 
->>>>>>> origin/master
         public ActionResult Index()
         {
             return View();
