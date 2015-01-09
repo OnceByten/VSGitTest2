527bdee43ada7056ddbe788b8698d268aa12c74f
Simon Gough <simon@oncebyten.com>
2015-01-09 15:04:49 +0000

    test home update
    also added a  person class

---
+++ Models/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VSGitTest2.Models
+{
+    public class Person
+    {
+        public string name { get; set;}
+
+        public Person() {
+
+            this.name = String.Empty;
+        }
+    }
+}
